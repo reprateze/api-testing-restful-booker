499276d7ef5b188c18f3ec3aafd143ff875b2f2c
reprateze <reprateze@gmail.com>
2026-09-13 22:08:06 -0300

    testes implementados: delete, update, getbook id e create

---
+++ utils/payloads/update_payload.py
@@ -0,0 +1,11 @@
+update_payload = {
+    "firstname": "Update",
+    "lastname": "name",
+    "totalprice": 120,
+    "depositpaid": True,
+    "bookingdates": {
+        "checkin": "2026-09-12",
+        "checkout": "2026-09-16"
+    },
+    "additionalneeds": "Lunch"
+}
